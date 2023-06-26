e19226341238a20c83433a7a385a3ba72d4af06b
Oluwaseun Adeyeri <adeyeriseun0@gmail.com>
2023-06-26 04:21:01 -0700

    updated header file

--- main.h
+++ main.h
@@ -2,8 +2,21 @@
 #define MAIN_H
 #include <stdio.h>
 #include <stdarg.h>
-#include <limit.h>
-
+/**
+ * struct start - defines a structure
+ * @sys: the operator
+ * @f: the function associated
+ */
+typedef struct conver_s
+{
+	char *sys;
+	int (*f)(va_list);
+} conver_t;
+int print_char(va_list arg_list);
+int print_string(va_list arg_list);
+int print_percent(va_list arg_list);
+int parser(const char *format, conver_t f_list[], va_list arg_list);
 int _printf(const char *format, ... );
+
 
 #endif
